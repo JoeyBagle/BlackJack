38612ef7e7be75a57dabd3aff9ec5eba661b2198
Joseph Beegle <71575543+JoeyBagle@users.noreply.github.com>
2024-05-07 12:53:28 -0700

    Update GenerateData.py

--- OOP/GenerateData.py
+++ OOP/GenerateData.py
@@ -5,14 +5,16 @@ model to use for learning and testing.
 
 '''
 
-data_for_learning = [('Player Hand Value', 'Player State', 'Player Hand Soft', 'Dealer Up Card', 'Game Win')]
+data_for_learning = [('Player_Hand_Value', 'Player_State', 'Player_Hand_Soft', 'Dealer_Up_Card', 'Game_Win')]
 
 import Main
 import numpy as np
 
 def generate_data():
-    for i in range(3000):
-        game = Main.Game(100, 'random')
-        game.run()
-        data_for_learning.append((game.player.hand.value(), game.player.state, game.player.hand.soft, game.dealer.hand.cards[0].rank, game.check_win()))
+    for i in range(30000):
+        game = Main.Game(100, 'strategy_one')
+        winLoss = game.run()
+        data_for_learning.append((game.player.hand.value(), game.player.state, game.player.hand.soft, game.dealer.hand.cards[0].rank, winLoss > 100))
     np.savetxt('DataForLearning.csv', [p for p in data_for_learning], delimiter=',', fmt='%s')
+
+generate_data()
