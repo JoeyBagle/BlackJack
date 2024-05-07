2452b533fde3f66dcf0d389849d9b62fa564750f
Joseph Beegle <71575543+JoeyBagle@users.noreply.github.com>
2024-05-07 16:15:07 +0000

    Added a generate data file to create data for the machine learning model, also renamed the test file

---
+++ OOP/GenerateData.py
@@ -0,0 +1,18 @@
+'''
+
+Generate data for the Machine Learning
+model to use for learning and testing.
+
+'''
+
+data_for_learning = [('Player Hand Value', 'Player State', 'Player Hand Soft', 'Dealer Up Card', 'Game Win')]
+
+import Main
+import numpy as np
+
+def generate_data():
+    for i in range(3000):
+        game = Main.Game(100, 'strategy_one')
+        game.check_win()
+        data_for_learning.append((game.player.hand.value(), game.player.state, game.player.hand.soft, game.dealer.hand.cards[0].rank, game.check_win()))
+    np.savetxt('DataForLearning.csv', [p for p in data_for_learning], delimiter=',', fmt='%s')
